5f1b99934a05f58beb4389c5aeaff6b9a1c3e55a
Makomeworld07 <164092940+Makomeworld07@users.noreply.github.com>
2025-06-02 21:49:30 +1200

    Add files via upload
    I have created a GUI class - this has been taken from my book manager task and I have chnaged it to fit this task. Still needs work

--- GUI.java
+++ GUI.java
@@ -1,42 +1,60 @@
 import ecs100.*;
 /**
- * Write a description of class GUI here.
+ * Code has been taken from my book manager 
+ * GUI class
+ * Allows to add, print and search for pokemon card
+ * Will display details: Name and value
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Korimako
  */
 public class GUI
 {
-    // instance variables - replace the example below with your own
-    private Cards card; // declare cards instance
-    private Card cards; // declare card instance
-
+    // field
+    private Cards cards;     //declare cards instance
+    private Card card;      //declare cards instance
+    
     /**
      * Constructor for objects of class GUI
      */
     public GUI()
     {
         // initialise instance variables
-        cards = new Cards(); // intaniate the cards object
-        UI.initiliase(); // Creates the GUI
-        UI.addButton("print all", cards::printAll); // calls the printall method
-        UI.addButton("Add", this::addCard); // calls 
-        UI.addButton("Find", this::findCard);
+        cards = new Cards();           //instantiate the cards
+        
+        // Set up GUI
+        UI.initialise();
+        UI.addButton("Print All", cards::printAllCards);
+        UI.addButton("Add", cards::addCard);
+        UI.addButton("Find", cards::findCard);
         UI.addButton("Quit", UI::quit);
+        //Set up mouse
+        //UI.setMouseListener(this::doMouse);
+
     }
     
     /**
-     * Finds cards based on name
-     * Prints out name
-     */
-    public void findCard() {
-        String cardName = UI.askString("Name of pokemon");
-        if (card.findCard(cardName.toLowerCase())) {
-            UI.println("found card");
-            card = cards.getCard();
-            
+     * Select obj based on where the user clicks
+    
+    private void doMouse(String action, double x, double y)
+    {
+        if (action.equals("clicked")){
+            // check the loc of the x and y against loc of the obj
+            if ((x >= cards.getCard().getLeft()) && (x <= cards.getCard().getRight()) &&
+                (y >= cards.getCard().getTop()) && (y <= cards.getCard().getBottom())){
+                    cards.getCard().increaseLike();
+                    UI.println("Added Likes: " + books.getBook().getLike());
+                }
         }
     }
-        
+    */
+    
+    /**
+     * main routine
+     */
+    public static void main(String[] args) {
+        new GUI();
     }
-   
+    
+}
+
+
